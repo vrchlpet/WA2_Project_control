1751078304e3407f68f8ab09aa51b9164d7a1b79
vrchlpet <vrchlpet@fel.cvut.cz>
2012-04-16 15:17:35 +0200

    added LogInServlet, LogIn.jsp, web.xml changed

--- src/ProjectControl/src/org/cvut/wa2/projectcontrol/ProjectControlServlet.java
+++ src/ProjectControl/src/org/cvut/wa2/projectcontrol/ProjectControlServlet.java
@@ -3,11 +3,22 @@ package org.cvut.wa2.projectcontrol;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 @SuppressWarnings("serial")
 public class ProjectControlServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		
+		if ( user != null) {
+			resp.sendRedirect("/LogIn.jsp");
+		} else {
+			resp.sendRedirect("/TeamsTask.jsp");
+		}
 	}
 }
