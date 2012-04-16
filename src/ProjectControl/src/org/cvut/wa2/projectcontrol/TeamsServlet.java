276b9f43814aebd6a9c9136633cf45a6739a6d30
vtomcanyi <viliam.tomcanyi@gmail.com>
2012-04-16 15:45:07 +0200

    fixed: TeamsServlet added: TaskServlet

--- src/ProjectControl/src/org/cvut/wa2/projectcontrol/TeamsServlet.java
+++ src/ProjectControl/src/org/cvut/wa2/projectcontrol/TeamsServlet.java
@@ -20,7 +20,6 @@ public class TeamsServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		super.doGet(req, resp);
 		UserService service = UserServiceFactory.getUserService();
 		User user = service.getCurrentUser();
 		if(user!= null){
