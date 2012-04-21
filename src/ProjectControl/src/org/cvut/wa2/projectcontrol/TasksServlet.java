5b97aa64f0fd50114e4b1d9eac21c26dc871153c
vtomcanyi <viliam.tomcanyi@gmail.com>
2012-04-21 13:39:58 +0200

    Add: TaskServlet

---
+++ src/ProjectControl/src/org/cvut/wa2/projectcontrol/TasksServlet.java
@@ -0,0 +1,33 @@
+package org.cvut.wa2.projectcontrol;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+public class TasksServlet extends HttpServlet{
+
+	private static final long serialVersionUID = -2239726230771945395L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		UserService service = UserServiceFactory.getUserService();
+		User user = service.getCurrentUser();
+		if(user!= null){
+			DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+			
+		}
+		else{
+			resp.sendRedirect("/projectcontrol");
+		}
+	}
+}
