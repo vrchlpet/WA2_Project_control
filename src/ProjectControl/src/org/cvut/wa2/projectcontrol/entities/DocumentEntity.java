2e7c2b29a4b63e84fb2a22ac375fb53acc708246
vtomcanyi <viliam.tomcanyi@gmail.com>
2012-05-05 22:02:20 +0200

    add and fix

---
+++ src/ProjectControl/src/org/cvut/wa2/projectcontrol/entities/DocumentEntity.java
@@ -0,0 +1,22 @@
+package org.cvut.wa2.projectcontrol.entities;
+
+public class DocumentEntity {
+private String docName;
+private String href;
+public String getHref() {
+	return href;
+}
+
+public void setHref(String href) {
+	this.href = href;
+}
+
+public String getDocName() {
+	return docName;
+}
+
+public void setDocName(String docName) {
+	this.docName = docName;
+}
+
+}
