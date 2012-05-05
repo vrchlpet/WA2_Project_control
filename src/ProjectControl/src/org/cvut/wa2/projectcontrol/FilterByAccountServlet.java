828758da18f8317ef4c87f25356670004b783af8
vtomcanyi <viliam.tomcanyi@gmail.com>
2012-05-06 00:09:55 +0200

    back add

--- src/ProjectControl/src/org/cvut/wa2/projectcontrol/FilterByAccountServlet.java
+++ src/ProjectControl/src/org/cvut/wa2/projectcontrol/FilterByAccountServlet.java
@@ -2,18 +2,28 @@ package org.cvut.wa2.projectcontrol;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 public class FilterByAccountServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		UserService userService = UserServiceFactory.getUserService();
+		RequestDispatcher disp = null;
+		if (userService.isUserLoggedIn()) {
+			String account = (String)req.getAttribute("account");
+			//TODO
+		}else{
+			resp.sendRedirect("/projectcontrol");
+		}
 	}
 
 	@Override
